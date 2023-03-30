a2a83100a77c1121f9f852e57b430ae851624130
Stephanie Wang <73036278+stcwang@users.noreply.github.com>
2023-03-30 16:12:48 -0400

    read main addresses

--- count_uniq_ip.py
+++ count_uniq_ip.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python3
 
+import json
 import sys
 
 if len(sys.argv) < 2:
@@ -8,8 +9,18 @@ if len(sys.argv) < 2:
 
 # '/datadrive/datasets/ripe/data-store.ripe.net/datasets/atlas-daily-dumps/traceroute-2023-01-27T2300_IPv6-only.jsonl'
 file = sys.argv[1]
+ipset = set()
 
 with open(file, "r") as f:
     line = f.readline()
     while line:
+        obj = json.loads(line)
+        if "dst_addr" in obj:
+            ipset.add(obj["dst_addr"])
+        if "src_addr" in obj:
+            ipset.add(obj["src_addr"])
+        if "from" in obj:
+            ipset.add(obj["from"])
         line = f.readline()
+
+print("Number of unique IPs found:", len(ipset))
