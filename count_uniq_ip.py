ad6a64d9ff005dd42713c4699e3cfaa1ff78d0c2
Stephanie Wang <73036278+stcwang@users.noreply.github.com>
2023-03-30 15:58:30 -0400

    read file line by line

--- count_uniq_ip.py
+++ count_uniq_ip.py
@@ -1,7 +1,15 @@
+#! /usr/bin/python3
+
 import sys
 
 if len(sys.argv) < 2:
     print("Usage: " + sys.argv[0] + "<traceroute file>")
     exit(1)
 
+# '/datadrive/datasets/ripe/data-store.ripe.net/datasets/atlas-daily-dumps/traceroute-2023-01-27T2300_IPv6-only.jsonl'
+file = sys.argv[1]
 
+with open(file, "r") as f:
+    line = f.readline()
+    while line:
+        line = f.readline()
